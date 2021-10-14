1cd99bb4fc86479cd1b39f333d5a19169b022c51
nuxion <nuxion@gmail.com>
2021-10-14 19:04:07 -0300

    correcion de words counter

--- cierre/words_counter.py
+++ cierre/words_counter.py
@@ -1,50 +1,28 @@
 import csv
 from collections import Counter
 from os import read
+import sys 
 
 # https://www.kaggle.com/gpreda/reddit-vaccine-myths?select=reddit_vm.csv
+filepath = "/home/nuxion/Projects/python-wrjhh/ejercicio_cierre"
+with open(f"{filepath}/stop_en.txt", "r") as f:
+    _words = f.read()
 
-with open("../reddit_vm.csv", "r") as f:
+stop_words = _words.split()
+textos = []
+with open(f"{filepath}/reddit_vm.csv", "r") as f:
     csvreader = csv.reader(f)
     rows = []
     
     for row in csvreader:
-        rows.append(row)
+        textos.append(row[6])
 
-body = []
-for i in rows:
-    body.append(i[6])
-
-# Creo una nueva lista
 palabras = []
-# Itero sobre mi lista creada del body for words in body:
-for words in body:
-    #hago un split de los strings en una nueva lista de palabras
-    listapalabras = words.split()
-    #extend newlist to include all itemWords
-    palabras.extend(listapalabras)
-print(palabras[3])
+for frase in textos:
+    lista_palabras = frase.split()
+    for palabra in lista_palabras:
+        if palabra.lower() not in stop_words:
+            palabras.append(palabra)
 
-
-with open("../stop_en.txt", "r") as f:
-    text = f.read()
-
-
-#c = Counter()
-#numero = Counter(palabras).most_common(5)
-
-top_5 = []
-for element in text:
-    if element in palabras:
-        top_5.append(element)
-    else
-    
-
-
-
-#with open("reddit_vm.csv", "r") as f:
-#    texto = f.readlines()
-#    for line in texto:
-#        row = line.split(",")
-#        print(row[0], row[1])
-#
+top = Counter(palabras).most_common(5)
+print(top)
